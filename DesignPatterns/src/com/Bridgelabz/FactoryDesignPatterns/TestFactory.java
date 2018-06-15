333e3fd5885bda91431c58f5563f594b0bc0adab
Chaitra111 <111chaitra@gmail>
2018-06-15 19:37:06 +0530

    15/06/2018

---
+++ DesignPatterns/src/com/Bridgelabz/FactoryDesignPatterns/TestFactory.java
@@ -0,0 +1,11 @@
+package com.Bridgelabz.FactoryDesignPatterns;
+
+public class TestFactory {
+
+	public static void main(String[] args) {
+		Computer pc=ComputerFactory.getComputer("pc", "2GB", "500GB", "2.5GHz");
+		Computer server=ComputerFactory.getComputer("Server", "8GB", "1TB", "2.4GHz");
+		System.out.println("PC: "+pc);
+		System.out.println("Server: "+server);
+	}
+}
---
+++ DesignPatterns/src/com/Bridgelabz/singletonpatterns/TestClass.java
@@ -0,0 +1,25 @@
+package com.Bridgelabz.singletonpatterns;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		
+		/*System.out.println(EagerInitializedSingleton.getInstance());
+		System.out.println(EagerInitializedSingleton.getInstance());
+		System.out.println(EagerInitializedSingleton.getInstance());
+		 */		
+		/*System.out.println(LazyInitializedSingleton.getInstance());
+		System.out.println(LazyInitializedSingleton.getInstance());
+		System.out.println(LazyInitializedSingleton.getInstance());*/
+		
+		/*
+		System.out.println(StaticBlockSingleton.getInstance());
+		System.out.println(StaticBlockSingleton.getInstance1());
+		System.out.println(StaticBlockSingleton.getInstance());*/
+		
+		/*System.out.println(ThreadSafeSingleton.getInstance().hashCode());*/
+		System.out.println(ThreadSafeSingletonUsingSynchronized.getInstance().hashCode());
+		
+	}
+
+}
